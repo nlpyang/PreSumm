0e0905d1dea7bf05acee0c0cd6922c3c9d7071ca
pku7yang <pku7yang@gmail.com>
2019-09-02 14:21:21 +0100

    change max_pos to 512

--- src/train.py
+++ src/train.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     parser.add_argument("-batch_size", default=140, type=int)
     parser.add_argument("-test_batch_size", default=200, type=int)
 
-    parser.add_argument("-max_pos", default=800, type=int)
+    parser.add_argument("-max_pos", default=512, type=int)
     parser.add_argument("-use_interval", type=str2bool, nargs='?',const=True,default=True)
     parser.add_argument("-large", type=str2bool, nargs='?',const=True,default=False)
     parser.add_argument("-load_from_extractive", default='', type=str)
