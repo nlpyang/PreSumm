d6f77c8d46636c67177e0a28df503d8a2355061d
pku7yang@gmail.com <910528@Ly>
2019-10-01 21:04:01 +0800

    add roberta

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -181,7 +181,7 @@ class AbsSummarizer(nn.Module):
         self.args = args
         self.device = device
         self.bert = Bert(args.bert_version, args.temp_dir, args.finetune_bert)
-
+        self.bert.model.resize_token_embeddings(2)
         if bert_from_extractive is not None:
             self.bert.model.load_state_dict(
                 dict([(n[11:], p) for n, p in bert_from_extractive.items() if n.startswith('bert.model')]), strict=True)
