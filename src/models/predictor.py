94d6b427f06e1574659b84ec6d6b28df67ce4c14
pku7yang@gmail.com <910528@Ly>
2019-10-08 20:22:27 +0800

    add roberta

--- src/models/predictor.py
+++ src/models/predictor.py
@@ -111,7 +111,7 @@ class Translator(object):
             #                           attn[b], pred_score[b], gold_sent,
             #                           gold_score[b])
             # src = self.spm.DecodeIds([int(t) for t in translation_batch['batch'].src[0][5] if int(t) != len(self.spm)])
-            raw_src = [self.vocab.ids_to_tokens[int(t)] for t in src[b]][:500]
+            raw_src = self.vocab.decode([int(t) for t in src[b]][:500])
             raw_src = ' '.join(raw_src)
             translation = (pred_sents, gold_sent, raw_src)
             # translation = (pred_sents[0], gold_sent)
