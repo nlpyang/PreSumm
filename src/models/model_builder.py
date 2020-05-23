c499d5521fddee475332e29ad9dc0b558d399a57
Yang Liu <pku7yang@gmail.com>
2020-05-23 16:35:39 +0800

    fp16

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -21,6 +21,8 @@ def build_optim(args, model, checkpoint):
                 for k, v in state.items():
                     if torch.is_tensor(v):
                         state[k] = v.cuda()
+                        if args.fp16:
+                            state[k] = state[k].half()
 
         if (optim.method == 'adam') and (len(optim.optimizer.state) < 1):
             raise RuntimeError(
@@ -35,68 +37,6 @@ def build_optim(args, model, checkpoint):
             warmup_steps=args.warmup_steps)
 
     optim.set_parameters(list(model.named_parameters()))
-
-
-    return optim
-
-def build_optim_bert(args, model, checkpoint):
-    """ Build optimizer """
-
-    if checkpoint is not None:
-        optim = checkpoint['optims'][0]
-        saved_optimizer_state_dict = optim.optimizer.state_dict()
-        optim.optimizer.load_state_dict(saved_optimizer_state_dict)
-        if args.visible_gpus != '-1':
-            for state in optim.optimizer.state.values():
-                for k, v in state.items():
-                    if torch.is_tensor(v):
-                        state[k] = v.cuda()
-
-        if (optim.method == 'adam') and (len(optim.optimizer.state) < 1):
-            raise RuntimeError(
-                "Error: loaded Adam optimizer from existing model" +
-                " but optimizer state is empty")
-
-    else:
-        optim = Optimizer(
-            args.optim, args.lr_bert, args.max_grad_norm,
-            beta1=args.beta1, beta2=args.beta2,
-            decay_method='noam',
-            warmup_steps=args.warmup_steps_bert)
-
-    params = [(n, p) for n, p in list(model.named_parameters()) if n.startswith('bert.model')]
-    optim.set_parameters(params)
-
-
-    return optim
-
-def build_optim_dec(args, model, checkpoint):
-    """ Build optimizer """
-
-    if checkpoint is not None:
-        optim = checkpoint['optims'][1]
-        saved_optimizer_state_dict = optim.optimizer.state_dict()
-        optim.optimizer.load_state_dict(saved_optimizer_state_dict)
-        if args.visible_gpus != '-1':
-            for state in optim.optimizer.state.values():
-                for k, v in state.items():
-                    if torch.is_tensor(v):
-                        state[k] = v.cuda()
-
-        if (optim.method == 'adam') and (len(optim.optimizer.state) < 1):
-            raise RuntimeError(
-                "Error: loaded Adam optimizer from existing model" +
-                " but optimizer state is empty")
-
-    else:
-        optim = Optimizer(
-            args.optim, args.lr_dec, args.max_grad_norm,
-            beta1=args.beta1, beta2=args.beta2,
-            decay_method='noam',
-            warmup_steps=args.warmup_steps_dec)
-
-    params = [(n, p) for n, p in list(model.named_parameters()) if not n.startswith('bert.model')]
-    optim.set_parameters(params)
 
 
     return optim
@@ -132,73 +72,21 @@ class Bert(nn.Module):
         return top_vec
 
 
-class ExtSummarizer(nn.Module):
-    def __init__(self, args, device, checkpoint):
-        super(ExtSummarizer, self).__init__()
-        self.args = args
-        self.device = device
-        self.bert = Bert(args.large, args.temp_dir, args.finetune_bert)
-
-        self.ext_layer = ExtTransformerEncoder(self.bert.model.config.hidden_size, args.ext_ff_size, args.ext_heads,
-                                               args.ext_dropout, args.ext_layers)
-        if (args.encoder == 'baseline'):
-            bert_config = BertConfig(self.bert.model.config.vocab_size, hidden_size=args.ext_hidden_size,
-                                     num_hidden_layers=args.ext_layers, num_attention_heads=args.ext_heads, intermediate_size=args.ext_ff_size)
-            self.bert.model = BertModel(bert_config)
-            self.ext_layer = Classifier(self.bert.model.config.hidden_size)
-
-        if(args.max_pos>512):
-            my_pos_embeddings = nn.Embedding(args.max_pos, self.bert.model.config.hidden_size)
-            my_pos_embeddings.weight.data[:512] = self.bert.model.embeddings.position_embeddings.weight.data
-            my_pos_embeddings.weight.data[512:] = self.bert.model.embeddings.position_embeddings.weight.data[-1][None,:].repeat(args.max_pos-512,1)
-            self.bert.model.embeddings.position_embeddings = my_pos_embeddings
-
-
-        if checkpoint is not None:
-            self.load_state_dict(checkpoint['model'], strict=True)
-        else:
-            if args.param_init != 0.0:
-                for p in self.ext_layer.parameters():
-                    p.data.uniform_(-args.param_init, args.param_init)
-            if args.param_init_glorot:
-                for p in self.ext_layer.parameters():
-                    if p.dim() > 1:
-                        xavier_uniform_(p)
-
-        self.to(device)
-
-    def forward(self, src, segs, clss, mask_src, mask_cls):
-        top_vec = self.bert(src, segs, mask_src)
-        sents_vec = top_vec[torch.arange(top_vec.size(0)).unsqueeze(1), clss]
-        sents_vec = sents_vec * mask_cls[:, :, None].float()
-        sent_scores = self.ext_layer(sents_vec, mask_cls).squeeze(-1)
-        return sent_scores, mask_cls
-
-
 class AbsSummarizer(nn.Module):
-    def __init__(self, args, device, checkpoint=None, bert_from_extractive=None):
+    def __init__(self, args, device, checkpoint=None):
         super(AbsSummarizer, self).__init__()
         self.args = args
         self.device = device
         self.bert = Bert(args.large, args.temp_dir, args.finetune_bert)
 
-        if bert_from_extractive is not None:
-            self.bert.model.load_state_dict(
-                dict([(n[11:], p) for n, p in bert_from_extractive.items() if n.startswith('bert.model')]), strict=True)
 
-        if (args.encoder == 'baseline'):
-            bert_config = BertConfig(self.bert.model.config.vocab_size, hidden_size=args.enc_hidden_size,
-                                     num_hidden_layers=args.enc_layers, num_attention_heads=8,
-                                     intermediate_size=args.enc_ff_size,
-                                     hidden_dropout_prob=args.enc_dropout,
-                                     attention_probs_dropout_prob=args.enc_dropout)
-            self.bert.model = BertModel(bert_config)
+        bert_config = BertConfig(self.bert.model.config.vocab_size, hidden_size=args.enc_hidden_size,
+                                 num_hidden_layers=args.enc_layers, num_attention_heads=8,
+                                 intermediate_size=args.enc_ff_size,
+                                 hidden_dropout_prob=args.enc_dropout,
+                                 attention_probs_dropout_prob=args.enc_dropout)
+        self.bert.model = BertModel(bert_config)
 
-        if(args.max_pos>512):
-            my_pos_embeddings = nn.Embedding(args.max_pos, self.bert.model.config.hidden_size)
-            my_pos_embeddings.weight.data[:512] = self.bert.model.embeddings.position_embeddings.weight.data
-            my_pos_embeddings.weight.data[512:] = self.bert.model.embeddings.position_embeddings.weight.data[-1][None,:].repeat(args.max_pos-512,1)
-            self.bert.model.embeddings.position_embeddings = my_pos_embeddings
         self.vocab_size = self.bert.model.config.vocab_size
         tgt_embeddings = nn.Embedding(self.vocab_size, self.bert.model.config.hidden_size, padding_idx=0)
         if (self.args.share_emb):
@@ -236,6 +124,9 @@ class AbsSummarizer(nn.Module):
                 self.generator[0].weight = self.decoder.embeddings.weight
 
         self.to(device)
+        if args.fp16:
+            self.half()
+
 
     def forward(self, src, tgt, segs, clss, mask_src, mask_tgt, mask_cls):
         top_vec = self.bert(src, segs, mask_src)
